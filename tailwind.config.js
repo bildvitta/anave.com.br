6ba79cc13d8c18edb8b9098410c93206c3af5e7b
Guilherme Queiróz <guilherme.websource@gmail.com>
2020-09-08 19:37:26 -0300

    tailwind.conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,18 @@ module.exports = {
       padding: '1em'
     },
 
+    gradientColorStops: theme => ({
+      primary: '#7F2BED',
+      secondary: '#401677',
+      'primary-hover': '#8A46EA',
+      'secondary-hover': '#452375'
+    }),
+
+    textColor: theme => ({
+      white: '#fff',
+      'nave-purple': '#7C34EF'
+    }),
+
     fontFamily: {
       montserrat: ['Montserrat', 'sans-serif']
     }
