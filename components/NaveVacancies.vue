196c6a024b068951ffc8380bd3b3f669e4e11bc4
Rafael Martins de Castro <42079080+Far3llProject@users.noreply.github.com>
2020-09-16 16:54:11 -0300

    update review

--- components/NaveVacancies.vue
+++ components/NaveVacancies.vue
@@ -10,17 +10,17 @@
 
 <script>
 const vacancies = [
-  { title: 'php', resume: 'Pessoa ', description: 'é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um.modelos de tipos.', img: require('../assets/img/PHP-logo.png') },
-  { title: 'Rubi on Rails', resume: 'Pessoa desenvolvedora backend', description: 'é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um.modelos de tipos.', img: require('../assets/img/Ruby_On_Rails_Logo.png') },
-  { title: 'VUE.JS', resume: 'Pessoa desenvolvedora Frontend', description: 'é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um.modelos de tipos.', img: require('../assets/img/vuejs.png') },
-  { title: 'php', resume: 'Pessoa desenvolvedora backend', description: 'é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um.modelos de tipos.', img: require('../assets/img/PHP-logo.png') },
-  { title: 'VUE.JS', resume: 'Pessoa desenvolvedora Frontend Frontend', description: 'é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um.modelos de tipos.', img: require('../assets/img/PHP-logo.png') },
-  { title: 'VUE.JS', resume: 'Pessoa desenvolvedora Frontend', description: 'é simplesmente uma simulação .', img: require('../assets/img/PHP-logo.png') },
-  { title: 'VUE.JS', resume: 'Pessoa desenvolvedora Frontend', description: 'é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um.modelos de tipos.', img: require('../assets/img/PHP-logo.png') },
-  { title: 'VUE.JS', resume: 'Pessoa desenvolvedora Frontend', description: 'é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um.modelos de tipos.', img: require('../assets/img/PHP-logo.png') },
-  { title: 'VUE.JS', resume: 'Pessoa desenvolvedora Frontend', description: 'é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um.modelos de tipos.', img: require('../assets/img/PHP-logo.png') },
-  { title: 'VUE.JS', resume: 'Pessoa desenvolvedora Frontend', description: 'é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um.modelos de tipos.', img: require('../assets/img/PHP-logo.png') },
-  { title: 'VUE.JS', resume: 'Pessoa desenvolvedora Frontend', description: 'é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um.modelos de tipos.', img: require('../assets/img/PHP-logo.png') }
+  { title: 'php', resume: 'backend ', description: 'é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um.modelos de tipos.', img: require('../assets/img/PHP-logo.png') },
+  { title: 'Rubi on Rails', resume: ' backend', description: 'é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um.modelos de tipos.', img: require('../assets/img/Ruby_On_Rails_Logo.png') },
+  { title: 'VUE.JS', resume: ' Frontend', description: 'é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um.modelos de tipos.', img: require('../assets/img/vuejs.png') },
+  { title: 'php', resume: ' ', description: 'é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um.modelos de tipos.', img: require('../assets/img/PHP-logo.png') },
+  { title: 'VUE.JS', resume: ' Frontend Frontend', description: 'é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um.modelos de tipos.', img: require('../assets/img/PHP-logo.png') },
+  { title: 'VUE.JS', resume: ' Frontend', description: 'é simplesmente uma simulação .', img: require('../assets/img/PHP-logo.png') },
+  { title: 'VUE.JS', resume: ' Frontend', description: 'é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um.modelos de tipos.', img: require('../assets/img/PHP-logo.png') },
+  { title: 'VUE.JS', resume: ' Frontend', description: 'é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um.modelos de tipos.', img: require('../assets/img/PHP-logo.png') },
+  { title: 'VUE.JS', resume: ' Frontend', description: 'é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um.modelos de tipos.', img: require('../assets/img/PHP-logo.png') },
+  { title: 'VUE.JS', resume: ' Frontend', description: 'é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um.modelos de tipos.', img: require('../assets/img/PHP-logo.png') },
+  { title: 'VUE.JS', resume: ' Frontend', description: 'é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um.modelos de tipos.', img: require('../assets/img/PHP-logo.png') }
 ]
 
 export default {
