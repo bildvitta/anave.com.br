3c3b1e65f0694f41222032231b48e5e02179c40a
Guilherme Queiróz <guilherme.websource@gmail.com>
2020-09-17 18:06:09 -0300

    meta lang

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,10 +19,14 @@ export default {
   */
   head: {
     title: process.env.npm_package_name || '',
+    htmlAttrs: {
+      lang: 'pt-br'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { 'http-equiv': 'Content-Language', content: 'pt-br' },
       { property: 'og:url', content: 'https://bildvitta.github.io/nave' },
       { property: 'og:title', content: 'Bild Vitta | NAVE' },
       { property: 'og:description', content: 'Desenhamos e lançamos produtos digitais que transformam empresas.' },
