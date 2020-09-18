3f60ff7c0aca0af35945787c684506a62ee3f907
Guilherme Queiróz <guilherme.websource@gmail.com>
2020-09-18 20:15:57 -0300

    adicionando modernizr webp lib

--- components/NaveHome.vue
+++ components/NaveHome.vue
@@ -37,7 +37,7 @@
   }
 
   &__image {
-    background-image: url('../assets/img/imagem_home_rocket.jpg');
+    background-image: url('../assets/img/imagem_home_rocket_small.webp');
     background-position: 0 -100px;
     background-repeat: no-repeat;
     background-size: cover;
