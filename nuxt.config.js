7686655aa18279fc300863b8245e1fe6635afe0e
Guilherme Queiróz <guilherme.websource@gmail.com>
2020-09-18 16:22:16 -0300

    link para og

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,7 @@ export default {
       { property: 'og:url', content: 'https://bildvitta.github.io/nave' },
       { property: 'og:title', content: 'Bild Vitta | NAVE' },
       { property: 'og:description', content: 'Desenhamos e lançamos produtos digitais que transformam empresas.' },
-      { property: 'og:image', content: 'https://bildvitta.github.io/nave/og-image.jpg' },
+      { property: 'og:image', content: 'https://bildvitta.github.io/nave/imagem_home_rocket.jpg' },
       { property: 'og:type', content: 'website' },
       { property: 'og:locale', content: 'pt-BR' }
     ],
