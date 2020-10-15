861b84d6bc3618f9c63126c553d82c10e16c1749
Rafael Martins de Castro <42079080+Far3llProject@users.noreply.github.com>
2020-10-15 17:58:26 -0300

    update text

--- components/NaveAbout.vue
+++ components/NaveAbout.vue
@@ -12,10 +12,10 @@
       </div>
 
       <div class="col-span-12 lg:col-span-5 lg:col-start-8 lg:mt-0 mt-10 nave-about__text-wrapper">
-        <h2 class="font-bold text-5xl text-white">Vamos falar sobre propósito?</h2>
+        <h2 class="font-bold text-5xl text-white">O universo é o nosso quintal</h2>
         <p class="mt-4 text-base text-white">
-          é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o
-          século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos.
+          Estamos crescendo. E através de uma nova cultura de inovação e uma inteligência estratégica, estamos mudando o cenário de desenvolvimento e tecnologia no interior de São Paulo, aprimorando talentos e criando experiências para melhorar milhares de vidas. Tudo isso, porque acreditamos que a verdadeira inovação está nas pessoas e não na tecnologia.
+          A NAVE já está em órbita, rumo ao desconhecido e inspirador futuro!
         </p>
         <img alt="Bild Vitta - NAVE" class="mt-6" src="../assets/img/Bild_Vitta.svg">
       </div>
--- components/NaveSpaceship.vue
+++ components/NaveSpaceship.vue
@@ -4,8 +4,7 @@
       <div class="col-end-12 col-start-1 lg:col-end-11 lg:col-start-3 lg:text-center">
         <h2 class="font-bold mb-4 nave-spaceship__title text-4xl">Espaço Nave</h2>
         <p class="block mb-10 mx-auto text-base text-gray-800">
-          é simplesmente uma simulação de texto da indústria tipográfica e de
-          impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma
+          Com um andar inteiro dedicado à construção de um ambiente plural, criativo e inovador, a NAVE está de portas abertas para receber toda a comunidade de tecnologia e promover a transformação do futuro.
         </p>
       </div>
       <div v-if="renders.spaceShip" class="col-span-12 flex flex-wrap justify-between nave-spaceship__gallery">
